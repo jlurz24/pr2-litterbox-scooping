87f44c3bda88cdae571b2d9d9eca658e9be4e0cd
Joshua <jlurz24@gmail.com>
2012-02-28 22:02:52 -0500

    Get movement to the litterbox working

--- scripts/scoop_litterbox_sm.py
+++ scripts/scoop_litterbox_sm.py
@@ -2,20 +2,12 @@ import roslib; roslib.load_manifest('litterbox')
 import rospy
 import smach
 import smach_ros
+import traceback
+
 from litterbox.msg import MoveToPositionAction
 from smach_ros import SimpleActionState, MonitorState
 from litterbox.msg import MoveToPositionGoal
 from geometry_msgs.msg import PointStamped
-
-# define state DetectLitterbox
-class DetectLitterbox(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['litterbox_detected','litterbox_not_detected'])
-
-    def execute(self, userdata):
-        rospy.loginfo('Executing state DetectLitterbox')
-        # Listen for messages here
-        return 'litterbox_detected';
 
 # main
 def main():
@@ -23,6 +15,7 @@ def main():
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['success', 'failure'])
+    sm.userdata.litterbox_position = None
 
     sis = smach_ros.IntrospectionServer('litterbox', sm, '/LITTERBOX_ROOT')
     sis.start()
@@ -31,24 +24,28 @@ def main():
     with sm:
         
         def move_to_litterbox_cb(userdata, goal):
-          goal.position = userdata.litterbox_position
+          rospy.loginfo("Inside move to lb callback")
+          goal = MoveToPositionGoal()
+          goal.position.point = userdata.litterbox_position
           return goal
 
-        def detect_cb(userdata, message):
-          rospy.loginfo('message: ' + message)
-          userdata.litterbox_position = message.position
-          return false # proceed
+        def detect_cb(userdata, msg):
+          rospy.loginfo("Inside detection callback")
+          sm.userdata.litterbox_position = msg.point
+          return False
 
         # Add states to the container
-        smach.StateMachine.add('DETECT_LITTERBOX', MonitorState('litterbox_location', PointStamped, detect_cb),
-                               transitions={'valid':'MOVE_TO_LITTERBOX', 
-                                            'preempted': 'DETECT_LITTERBOX',
-                                            'invalid':'failure'})
+        smach.StateMachine.add('DETECT_LITTERBOX', 
+                                MonitorState('object_location/Litterbox', PointStamped, detect_cb, 100), 
+                                transitions = {'valid':'failure', 
+                                               'preempted': 'DETECT_LITTERBOX',
+                                               'invalid':'MOVE_TO_LITTERBOX'})
+
         smach.StateMachine.add('MOVE_TO_LITTERBOX', 
                                SimpleActionState('move_to_position',
                                MoveToPositionAction,
                                goal_cb=move_to_litterbox_cb,
-                               input_keys=[]), 
+                               input_keys=['litterbox_position']), 
                                transitions={'succeeded':'success',
                                             'preempted':'failure',
                                             'aborted': 'failure'})
