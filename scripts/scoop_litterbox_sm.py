576ad8bf1404db7414928ab7e864f64413482aa7
Joshua <jlurz24@gmail.com>
2012-02-28 22:29:06 -0500

    Add detecting and moving to trash to the state machine

--- scripts/scoop_litterbox_sm.py
+++ scripts/scoop_litterbox_sm.py
@@ -18,6 +18,7 @@ def main():
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['success', 'failure'])
     sm.userdata.litterbox_position = None
+    sm.userdata.trash_position = None
 
     sis = smach_ros.IntrospectionServer('litterbox', sm, '/LITTERBOX_ROOT')
     sis.start()
@@ -30,11 +31,24 @@ def main():
           goal = MoveToPositionGoal()
           goal.position.point = userdata.litterbox_position
           return goal
+ 
+        def move_to_trash_cb(userdata, goal):
+          rospy.loginfo("Inside move to trash callback")
+          goal = MoveToPositionGoal()
+          goal.position.point = userdata.trash_position
+          return goal
 
-        def detect_cb(userdata, msg):
-          rospy.loginfo("Inside detection callback")
+        
+        def detect_litterbox_cb(userdata, msg):
+          rospy.loginfo("Inside litterbox detection callback")
           sm.userdata.litterbox_position = msg.point
           return False
+        
+        def detect_trash_cb(userdata, msg):
+          rospy.loginfo("Inside trash detection callback")
+          sm.userdata.trash_position = msg.point
+          return False
+
 
         def scoop_litterbox_cb(userdata, goal):
           rospy.loginfo("Inside scoop litterbox callback")
@@ -43,7 +57,7 @@ def main():
 
         # Add states to the container
         smach.StateMachine.add('DETECT_LITTERBOX', 
-                                MonitorState('object_location/Litterbox', PointStamped, detect_cb, 100), 
+                                MonitorState('object_location/Litterbox', PointStamped, detect_litterbox_cb, 100), 
                                 transitions = {'valid':'failure', 
                                                'preempted': 'DETECT_LITTERBOX',
                                                'invalid':'MOVE_TO_LITTERBOX'})
@@ -62,6 +76,22 @@ def main():
                                ScoopLitterboxAction,
                                goal_cb=scoop_litterbox_cb,
                                input_keys=[]),
+                               transitions={'succeeded':'MOVE_TO_TRASH',
+                                            'preempted':'failure',
+                                            'aborted': 'failure'})
+
+            # Add states to the container
+        smach.StateMachine.add('DETECT_TRASH',
+                                MonitorState('object_location/Trash', PointStamped, detect_trash_cb, 100),
+                                transitions = {'valid':'failure',
+                                               'preempted': 'DETECT_TRASH',
+                                               'invalid':'MOVE_TO_TRASH'})
+
+        smach.StateMachine.add('MOVE_TO_TRASH',
+                               SimpleActionState('move_to_position',
+                               MoveToPositionAction,
+                               goal_cb=move_to_trash_cb,
+                               input_keys=['trash_position']),
                                transitions={'succeeded':'success',
                                             'preempted':'failure',
                                             'aborted': 'failure'})
