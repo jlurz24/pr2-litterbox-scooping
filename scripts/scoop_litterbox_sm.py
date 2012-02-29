a044a80d7deb49e027475acb0d1abd340cec3ec8
Joshua <jlurz24@gmail.com>
2012-02-28 22:13:32 -0500

    Add scooping action

--- scripts/scoop_litterbox_sm.py
+++ scripts/scoop_litterbox_sm.py
@@ -8,6 +8,8 @@ from litterbox.msg import MoveToPositionAction
 from smach_ros import SimpleActionState, MonitorState
 from litterbox.msg import MoveToPositionGoal
 from geometry_msgs.msg import PointStamped
+from litterbox.msg import ScoopLitterboxAction
+from litterbox.msg import ScoopLitterboxGoal
 
 # main
 def main():
@@ -34,6 +36,11 @@ def main():
           sm.userdata.litterbox_position = msg.point
           return False
 
+        def scoop_litterbox_cb(userdata, goal):
+          rospy.loginfo("Inside scoop litterbox callback")
+          goal = ScoopLitterboxGoal()
+          return goal
+
         # Add states to the container
         smach.StateMachine.add('DETECT_LITTERBOX', 
                                 MonitorState('object_location/Litterbox', PointStamped, detect_cb, 100), 
@@ -46,10 +53,18 @@ def main():
                                MoveToPositionAction,
                                goal_cb=move_to_litterbox_cb,
                                input_keys=['litterbox_position']), 
+                               transitions={'succeeded':'SCOOP_LITTERBOX',
+                                            'preempted':'failure',
+                                            'aborted': 'failure'})
+       
+        smach.StateMachine.add('SCOOP_LITTERBOX',
+                               SimpleActionState('scoop_litterbox',
+                               ScoopLitterboxAction,
+                               goal_cb=scoop_litterbox_cb,
+                               input_keys=[]),
                                transitions={'succeeded':'success',
                                             'preempted':'failure',
                                             'aborted': 'failure'})
-
     # Execute SMACH plan
     outcome = sm.execute()
     rospy.spin()
